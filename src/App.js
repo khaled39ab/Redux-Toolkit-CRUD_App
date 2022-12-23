582dc2ba987cea9f55b61d2586b7636769fa4e09
khaled39ab <khaled39ab@gmail.com>
2022-12-23 17:19:35 +0600

    added header navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './layouts/footer/Footer';
+import Header from './layouts/header/Header';
 
 function App() {
   return (
     <div>
+      <Header />
+      <Routes>
+        <Route></Route>
+      </Routes>
       <Footer />
     </div>
   );
--- src/layouts/header/Header.js
+++ src/layouts/header/Header.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './Header.css'
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='header-container'>
+            <div>
+                <h2 className='icon'>Redux CRUD App</h2>
+            </div>
+            <div>
+                <nav>
+                    <NavLink className='link' to={'/'}>Home</NavLink>
+                    <NavLink className='link' to={'/show-books'}>Show Books</NavLink>
+                    <NavLink className='link' to={'/add-book'}>Add Book</NavLink>
+                </nav>
+            </div>
         </div>
     );
 };
