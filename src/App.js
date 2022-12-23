3f24abf2a2f4dc673e45d0b471df2cd9e3a6276c
khaled39ab <khaled39ab@gmail.com>
2022-12-23 17:24:11 +0600

    added home & notFound page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './layouts/footer/Footer';
 import Header from './layouts/header/Header';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <div>
       <Header />
       <Routes>
-        <Route></Route>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
       <Footer />
     </div>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div style={{textAlign: 'center'}}>
+            <h1>404!!! <br />
+                Page not found</h1>
+        </div>
+    );
+};
+
+export default NotFound;
